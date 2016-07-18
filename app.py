9812a738b30534fe08a1915ab0d5e037fda4ac72
golden <120225883@qq.com>
2016-07-18 21:00:40 +0800

    搞定登陆了

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, session, request, jsonify
+# coding:utf-8
+from __future__ import unicode_literals, absolute_import, print_function
+from flask import Flask, render_template, session, request, jsonify, g
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
-import time
+import paramiko
+import base64
+from binascii import hexlify
+import getpass
+import os
+import select
+import socket
 import sys
+import time
+import traceback
+from paramiko.py3compat import input
+
 import paramiko
 
+# try:
+#     import interactive
+# except ImportError:
+#     from . import interactive
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
@@ -15,6 +31,57 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
+
+
+def agent_auth(transport, username):
+    """
+    Attempt to authenticate to the given transport using any of the private
+    keys available from an SSH agent.
+    """
+
+    agent = paramiko.Agent()
+    agent_keys = agent.get_keys()
+    if len(agent_keys) == 0:
+        return
+
+    for key in agent_keys:
+        print('Trying ssh-agent key %s' % hexlify(key.get_fingerprint()))
+        try:
+            transport.auth_publickey(username, key)
+            print('... success!')
+            return
+        except paramiko.SSHException:
+            print('... nope.')
+
+
+def manual_auth(username, hostname, password=None):
+    default_auth = 'p'
+    auth = default_auth
+
+    if auth == 'r':
+        default_path = os.path.join(os.environ['HOME'], '.ssh', 'id_rsa')
+        path = input('RSA key [%s]: ' % default_path)
+        if len(path) == 0:
+            path = default_path
+        try:
+            key = paramiko.RSAKey.from_private_key_file(path)
+        except paramiko.PasswordRequiredException:
+            password = getpass.getpass('RSA key password: ')
+            key = paramiko.RSAKey.from_private_key_file(path, password)
+        t.auth_publickey(username, key)
+    elif auth == 'd':
+        default_path = os.path.join(os.environ['HOME'], '.ssh', 'id_dsa')
+        path = input('DSS key [%s]: ' % default_path)
+        if len(path) == 0:
+            path = default_path
+        try:
+            key = paramiko.DSSKey.from_private_key_file(path)
+        except paramiko.PasswordRequiredException:
+            password = getpass.getpass('DSS key password: ')
+            key = paramiko.DSSKey.from_private_key_file(path, password)
+        t.auth_publickey(username, key)
+    else:
+        t.auth_password(username, password)
 
 
 def background_thread():
@@ -42,36 +109,116 @@ def terminal():
 
 
 @app.route('/client_login/', methods=['POST'])
-def client_connect():
-    host = request.form.get('host')
-    port = request.form.get('port')
-    session['conn_host']=host
-    session['port']=port
-    return 'success'
-
-
-@app.route('/client_login/', methods=['POST'])
 def client_login():
     user = request.form.get('user')
     password = request.form.get('password')
+    host = request.form.get('host')
+    port = request.form.get('port')
     ssh = paramiko.SSHClient()
-    ssh.connect()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # socketio.emit('new ssh client', {'data': {'host': host, 'port': int(port), 'user': user, 'password': password}},
+    #               namespace='/test')
+    try:
+        ssh.connect(hostname=b'zousj.cn', port=7726, username=b'root', password=b'www.zousj.cn@golsee7726')
+        # session['ssh'] = ssh # 不能赋给session
+        socketio.ssh = ssh
+        return jsonify({'data': 'login success', 'errors': 0})
+    except Exception as e:
+        print(e)
+        return jsonify({'data': '登陆失败', 'errors': 1})
 
 
-@app.route('/exec/', methods=['POST'])
+@app.route('/exec/', methods=['POST', 'GET'])
 def execs():
+    print(g.get('ssh', None))
     return jsonify({"result": "aaaa"})
 
 
 @socketio.on('my event', namespace='/test')
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    import subprocess
-    p = subprocess.Popen(message['data'], shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE,
-                         stdin=subprocess.PIPE)
-    r = p.stdout.read()
     emit('my response',
-         {'data': r.decode('gbk'), 'count': session['receive_count']})
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.on('ssh login', namespace='/test')
+def ssh_login(message):
+    hostname = message['host']
+    port = message['port']
+    username = message['user']
+    password = message['password']
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((hostname, int(port)))
+    except Exception as e:
+        emit('ssh login response', {'data': e.message})
+    try:
+        t = paramiko.Transport(sock)
+        try:
+            t.start_client()
+        except paramiko.SSHException:
+            emit('ssh login response', {'data': '*** SSH negotiation failed.'})
+        try:
+            keys = paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+        except IOError:
+            try:
+                keys = paramiko.util.load_host_keys(os.path.expanduser('~/ssh/known_hosts'))
+            except IOError:
+                print('*** Unable to open host keys file')
+                emit('ssh login response', {'data': '*** Unable to open host keys file'})
+
+        # check server's host key -- this is important.
+        key = t.get_remote_server_key()
+        if hostname not in keys:
+            print('*** WARNING: Unknown host key!')
+            emit('ssh login response', {'data': '*** WARNING: Unknown host key!'})
+        elif key.get_name() not in keys[hostname]:
+            print('*** WARNING: Unknown host key!')
+            emit('ssh login response', {'data': '*** WARNING: Unknown host key!'})
+        elif keys[hostname][key.get_name()] != key:
+            print('*** WARNING: Host key has changed!!!')
+            emit('ssh login response', {'data': '*** WARNING: Host key has changed!!!'})
+        else:
+            print('*** Host key OK.')
+            emit('ssh login response', {'data': '*** Host key OK.'})
+
+        # get username
+        if username == '':
+            default_username = getpass.getuser()
+            if len(username) == 0:
+                username = default_username
+    except Exception as e:
+        emit('ssh login response', {'data': e.message})
+        agent_auth(t, username)
+        if not t.is_authenticated():
+            t.auth_password(username, password)
+            # manual_auth(username, hostname)
+        if not t.is_authenticated():
+            print('*** Authentication failed. :(')
+            emit('ssh login response', {'data': '*** Authentication failed. :('})
+            t.close()
+        else:
+            emit('ssh login response', {'data': 'login success'})
+
+            #
+            # chan = t.open_session()
+            # chan.get_pty()
+            # chan.invoke_shell()
+            # print('*** Here we go!\n')
+            # interactive.interactive_shell(chan)
+            # chan.close()
+            # t.close()
+
+
+@socketio.on('exec command', namespace='/test')
+def exec_command(message):
+    try:
+        stdin, stdout, stderr = socketio.ssh.exec_command(message['data'])
+        emit('command result',
+             {'data': stderr.read() or stdout.read(), 'count': session['receive_count']})
+    except Exception as e:
+        emit('command result',
+             {'data': e.message, 'count': session['receive_count']})
 
 
 @socketio.on('my broadcast event', namespace='/test')
